e16c2901706a249f3897b8049639ebaebd0dfe59
Jesufemi-A <jesufemia1@gmail.com>
2024-01-17 04:27:10 +0100

    content reveal upon scrolling

--- interact.js
+++ interact.js
@@ -1,13 +1,16 @@
 "use strict";
 
-const clc = document.querySelector(".grey-section");
+// const clc = document.querySelector(".grey-section");
 const headerBodyCon = document.querySelector(".header-body-con");
 const nav = document.querySelector(".nav");
 const header = document.querySelector("header");
+const headerHeight = header.getBoundingClientRect().height;
+// console.log(headerHeight);
 
 const obsOption = {
   root: null,
   threshold: 0,
+  rootMargin: `-${headerHeight}px`,
 };
 
 const obsCallback = (entries) => {
@@ -25,4 +28,31 @@ const observer = new IntersectionObserver(obsCallback, obsOption);
 observer.observe(headerBodyCon);
 
 
+/* revealing elements on scroll */
 
+
+const allSection = document.querySelectorAll('.section')
+
+
+
+const sectionOption = {
+  root: null,
+  threshold: 0.2,
+}
+
+const sectionCallback = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section-hidden');
+
+  observer.unobserve(entry.target); 
+
+}
+
+const sectionObserver = new IntersectionObserver(sectionCallback, sectionOption)
+allSection.forEach((sec) => {
+  // console.log(sec);
+  sectionObserver.observe(sec);
+  sec.classList.add('section-hidden');
+}) 
